235b9cd3f03de9b2a12669de25740ef18954c2a8
Trent You <tyou@ucdavis.edu>
2017-11-21 22:24:09 -0800

    Set up start script for package.json

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ var MongoClient = require('mongodb').MongoClient
 var bodyParser = require('body-parser');
 var path = require('path');
 
+app.set('port', 6969)
+
+var api_key = process.env.API_KEY;
+
+app.get("/hello", function(request, response) {
+    return response.json({ "Hello" : "Trent" });
+})
+
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log("API Key:" + api_key);
 })
