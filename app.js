abf32e557f20775ae3c2cbac303f2621534d1aa8
Trent You <tyou@ucdavis.edu>
2017-11-23 23:20:26 -0800

    Added dovenv module to apply env variables to process.env automatically

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
+
 var express = require('express')
 var app = express()
 var MongoClient = require('mongodb').MongoClient
 var bodyParser = require('body-parser');
 var path = require('path');
+var port = 8888;
 
-app.set('port', 6969)
+app.set('port', port)
 
 var api_key = process.env.API_KEY;
 
@@ -12,8 +15,17 @@ app.get("/hello", function(request, response) {
     return response.json({ "Hello" : "Trent" });
 })
 
+function checkCrypto() {
+    console.log("Hello");
+}
+
+function run() {
+    setInterval(checkCrypto, 10000);
+}
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
   console.log("API Key:" + api_key);
+  run();
 })
